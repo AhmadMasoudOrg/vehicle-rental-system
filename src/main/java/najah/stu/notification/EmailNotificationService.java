1329ac461322cacc80588bb025f5e28e3804cc5b
ahmadfaedsalman123-dot <ahmadfaedsalman123@gmail.com>
2026-07-31 10:50:50 +0300

    Update EmailNotificationService.java

--- src/main/java/najah/stu/notification/EmailNotificationService.java
+++ src/main/java/najah/stu/notification/EmailNotificationService.java
@@ -1,17 +1,13 @@
 package najah.stu.notification;
 
-import java.util.logging.Logger;
-
 public class EmailNotificationService implements NotificationService {
 
-    private static final Logger LOGGER = Logger.getLogger(EmailNotificationService.class.getName());
-
     @Override
-    public void sendNotification(String recipient,String subject,String message) {
-        LOGGER.info("======= EMAIL =======");
-        LOGGER.info(() -> "To: " + recipient);
-        LOGGER.info(() -> "Subject: " + subject);
-        LOGGER.info(message);
-        LOGGER.info("=====================");
+    public void sendNotification(String recipient, String subject, String message) {
+        System.out.println("======= EMAIL =======");
+        System.out.println("To: " + recipient);
+        System.out.println("Subject: " + subject);
+        System.out.println(message);
+        System.out.println("=====================");
     }
 }
